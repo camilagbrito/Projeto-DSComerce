c519c6f543ea8fde99bfc22d3d8569c8fb3a7b19
Camila Brito <camilagbrito48@gmail.com>
2023-02-07 15:05:08 +0000

    OAuth2 JWT checkList

--- src/main/java/com/devsuperior/dscommerce/DscommerceApplication.java
+++ src/main/java/com/devsuperior/dscommerce/DscommerceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class DscommerceApplication{
+public class DscommerceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DscommerceApplication.class, args);
