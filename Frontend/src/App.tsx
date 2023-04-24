3b31eff713c12b3dba6662a288d2b13d3b2c4d18
Camila Brito <camilagbrito48@gmail.com>
2023-04-24 16:09:33 +0100

    Adding React Router DOM

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,11 +1,22 @@
-import Catalog from './routes/Catalog';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ProductDetails from './routes/ProductDetails';
+import ClientHome from './routes/ClientHome';
+import Catalog from './routes/Catalog';
 
 
-function App() {
-  return ( 
-    <ProductDetails />
+export default function App() {
+
+  return(
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<ClientHome />}>
+        <Route index element={<Catalog/>} />
+        <Route path="catalog" element={<Catalog/>} />
+        <Route path="product-details" element={<ProductDetails/>} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
   );
 }
 
-export default App;
--- Frontend/src/routes/ProductDetails/index.tsx
+++ Frontend/src/routes/ProductDetails/index.tsx
@@ -1,7 +1,6 @@
 import './styles.css';
 import ButtonInverse from '../../components/ButtonInverse';
 import ButtonPrimary from '../../components/ButtonPrimary';
-import HeaderClient from '../../components/HeaderClient';
 import ProductDetailsCard from '../../components/ProductDetailsCard';
 import { ProductDTO } from '../../models/product';
 
@@ -29,17 +28,14 @@ const product: ProductDTO = {
 
 export default function ProductDetails(){
     return (
-        <>
-        <HeaderClient />
         <main>
           <section id="product-details-section" className="dsc-container">
             <ProductDetailsCard product={product} />
             <div className="dsc-btn-page-container">
-              <ButtonPrimary />
-              <ButtonInverse />
+              <ButtonPrimary text="Comprar"/>
+              <ButtonInverse text="Início"/>
             </div>
           </section>
         </main>
-        </>
     );
 }
