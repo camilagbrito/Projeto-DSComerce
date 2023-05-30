3c497e722571d3dbe22e99044f84119e881ee933
Camila Brito <camilagbrito48@gmail.com>
2023-05-30 13:45:43 +0100

    Cart save/get

---
+++ Frontend/src/localstorage/cart-repository.ts
@@ -0,0 +1,12 @@
+import { json } from "react-router-dom";
+import { OrderDTO } from "../models/order";
+
+export function save(cart:OrderDTO){
+    const str = JSON.stringify(cart);
+    localStorage.setItem("com.devsuperior.dscommerce/Cart", str); 
+}
+
+export function get() : OrderDTO {
+    const str = localStorage.getItem("com.devsuperior.dscommerce/Cart") || '{"items" = []}';
+    return JSON.parse(str);
+}
