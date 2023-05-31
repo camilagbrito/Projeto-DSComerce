c230d10080565756630021c0b84384da2787d949
Camila Brito <camilagbrito48@gmail.com>
2023-05-31 11:25:40 +0100

    Increase Item

--- Frontend/src/services/cart-service.ts
+++ Frontend/src/services/cart-service.ts
@@ -1,6 +1,7 @@
 import { OrderDTO, OrderItemDTO } from "../models/order";
 import * as cartRepository from "../localstorage/cart-repository";
 import { ProductDTO } from "../models/product";
+import Cart from "../routes/ClientHome/Cart";
 
 export function saveCart(cart:OrderDTO){
     cartRepository.save(cart);
@@ -22,4 +23,13 @@ export function addProduct(product: ProductDTO){
 
 export function clearCart(){
     cartRepository.clear();
+}
+
+export function increaseItem(productId: number){
+    const cart = cartRepository.get();
+    const item = cart.items.find(x => x.productId == productId);
+    if(item){
+        item.quantity++;
+        cartRepository.save(cart);
+    }
 }
