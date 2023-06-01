1e263249fae76fcd5a3c252d00748af35537c02d
Camila Brito <camilagbrito48@gmail.com>
2023-06-01 12:04:20 +0100

    Context Cart Count

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,25 +1,32 @@
-
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import ProductDetails from './routes/ClientHome/ProductDetails';
-import ClientHome from './routes/ClientHome';
-import Catalog from './routes/ClientHome/Catalog';
-import Cart from './routes/ClientHome/Cart';
-
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import ProductDetails from "./routes/ClientHome/ProductDetails";
+import ClientHome from "./routes/ClientHome";
+import Catalog from "./routes/ClientHome/Catalog";
+import Cart from "./routes/ClientHome/Cart";
+import { useState } from "react";
+import { ContextCartCount } from "./utils/context-cart";
 
 export default function App() {
+  const [contextCartCount, setContextCartCount] = useState<number>(0);
 
-  return(
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<ClientHome />}>
-        <Route index element={<Catalog/>} />
-        <Route path="catalog" element={<Catalog/>} />
-        <Route path="cart" element={<Cart/>} />
-        <Route path="product-details/:productId" element={<ProductDetails/>} />
-      </Route>
-      <Route path="*" element={<Navigate to="/" />} />
-    </Routes>
-  </BrowserRouter>
+  return (
+    <ContextCartCount.Provider
+      value={{ contextCartCount, setContextCartCount }}
+    >
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<ClientHome />}>
+            <Route index element={<Catalog />} />
+            <Route path="catalog" element={<Catalog />} />
+            <Route path="cart" element={<Cart />} />
+            <Route
+              path="product-details/:productId"
+              element={<ProductDetails />}
+            />
+          </Route>
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </BrowserRouter>
+    </ContextCartCount.Provider>
   );
 }
-
