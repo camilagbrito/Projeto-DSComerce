b8489ee4bdf331e0e00627646c62a7bfe02e80bd
Camila Brito <camilagbrito48@gmail.com>
2023-06-20 10:52:11 +0100

    Place order

--- Frontend/src/services/order-service.ts
+++ Frontend/src/services/order-service.ts
@@ -1,5 +1,6 @@
 import { AxiosRequestConfig } from "axios";
 import { requestBackend } from "../utils/requests";
+import { OrderDTO } from "../models/order";
 
 
 export function findByIdRequest(id: number){
@@ -11,3 +12,12 @@ export function findByIdRequest(id: number){
     return requestBackend(config);
 }
 
+export function placeOrderRequest(cart: OrderDTO){
+    const config: AxiosRequestConfig = {
+        url: "/orders",
+        method: "POST",
+        withCredentials: true,
+        data: cart
+    }
+    return requestBackend(config);
+}
