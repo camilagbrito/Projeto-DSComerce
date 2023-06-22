739bc9b8cd3a74b9bd3c2d759526df30c2397cf9
Camila Brito <camilagbrito48@gmail.com>
2023-06-22 13:43:05 +0100

    Login form global message

--- Frontend/src/routes/ClientHome/Login/index.tsx
+++ Frontend/src/routes/ClientHome/Login/index.tsx
@@ -9,6 +9,7 @@ import FormInput from "../../../components/FormInput";
 export default function Login() {
   const navigate = useNavigate();
   const { setContextTokenPayload } = useContext(ContextToken);
+  const [submitResponseFail, setSubmitResponseFail] = useState(false);
 
   const [formData, setFormData] = useState<any>({
     username: {
@@ -35,6 +36,13 @@ export default function Login() {
 
   function handleSubmit(event: any) {
     event.preventDefault();
+    setSubmitResponseFail(false);
+    const formDataValidated = forms.dirtyAndValidateAll(formData);
+    if(forms.hasAnyInvalid(formDataValidated)){
+      setFormData(formDataValidated);
+      return;
+    }
+
     authService
       .loginRequest(forms.toValue(formData))
       .then((response) => {
@@ -43,7 +51,7 @@ export default function Login() {
         navigate("/cart");
       })
       .catch((error) => {
-        console.log("Erro no login", error);
+        setSubmitResponseFail(true);
       });
   }
 
@@ -80,7 +88,13 @@ export default function Login() {
                 />
               </div>
             </div>
-
+            {
+              submitResponseFail &&
+              <div className="dsc-form-global-error">
+                Usuário ou senha inválidos
+              </div>
+            }
+           
             <div className="dsc-login-form-buttons dsc-mt20">
               <button type="submit" className="dsc-btn dsc-btn-blue">
                 Entrar
